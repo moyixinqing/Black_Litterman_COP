1ff7f636863fd44cade06571f663265e0cdba4f9
Sabrinaxren <40002588+moyixinqing@users.noreply.github.com>
2019-02-17 19:38:25 -0600

    Michaud

--- BlackLitterman.py
+++ BlackLitterman.py
@@ -1,27 +1,11 @@
 import numpy as np
+from math import sqrt
 from scipy import linalg
+from collections import namedtuple
 
 class Blacklitterman(object):
-    """The base class for Black Litterman.
-    This function performs the Black-Litterman blending of the prior
-    and the views into a new posterior estimate of the returns as
-    described in the paper by He and Litterman.
-   
-    ----------
-     Inputs
-       delta  - Risk tolerance from the equilibrium portfolio
-       weq    - Weights of the assets in the equilibrium portfolio
-       sigma  - Prior covariance matrix
-       tau    - Coefficiet of uncertainty in the prior estimate of the mean (pi)
-       P      - Pick matrix for the view(s)
-       Q      - Vector of view returns
-       Omega  - Matrix of variance of the views (diagonal)
-    Outputs
-       Er     - Posterior estimate of the mean returns
-       w      - Unconstrained weights computed given the Posterior estimates
-            of the mean and covariance of returns.
-       lambda - A measure of the impact of each view on the posterior estimates.
-    """
+    """The base class for Black Litterman."""
+    
     def __init__(self,delta, weq, sigma, tau, P, Q, Omega):
         self.delta = delta
         self.weq = weq
@@ -32,7 +16,24 @@ class Blacklitterman(object):
         self.Omega = Omega
         
     def blacklitterman(self):
-
+      """ This function performs the Black-Litterman blending of the prior
+        and the views into a new posterior estimate of the returns as
+        described in the paper by He and Litterman.
+        ----------
+         Inputs
+           delta  - Risk tolerance from the equilibrium portfolio
+           weq    - Weights of the assets in the equilibrium portfolio
+           sigma  - Prior covariance matrix
+           tau    - Coefficiet of uncertainty in the prior estimate of the mean (pi)
+           P      - Pick matrix for the view(s)
+           Q      - Vector of view returns
+           Omega  - Matrix of variance of the views (diagonal)
+        Outputs
+           Er     - Posterior estimate of the mean returns
+           w      - Unconstrained weights computed given the Posterior estimates
+                of the mean and covariance of returns.
+           lambda - A measure of the impact of each view on the posterior estimates.
+      """
       # Reverse optimize and back out the equilibrium returns
       # This is formula (12) page 6.
       pi = self.weq.dot(self.sigma * self.delta)
@@ -89,20 +90,66 @@ class Blacklitterman(object):
         return [er, w, lmbda]
     
     def bl_omega(self, conf, P, Sigma):
-        """ idz_omega
-        #   This function computes the Black-Litterman parameters Omega from
-        #   an Idzorek confidence.
-        # Inputs
-        #   conf   - Idzorek confidence specified as a decimal (50% as 0.50)
-        #   P      - Pick matrix for the view
-        #   Sigma  - Prior covariance matrix
-        # Outputs
-        #   omega  - Black-Litterman uncertainty/confidence parameter
+        """ Idzorek_omega
+        This function computes the Black-Litterman parameters Omega from
+           an Idzorek confidence.
+        ----------
+         Inputs
+           conf   - Idzorek confidence specified as a decimal (50% as 0.50)
+           P      - Pick matrix for the view
+           Sigma  - Prior covariance matrix
+         Outputs
+           omega  - Black-Litterman uncertainty/confidence parameter
         """
         alpha = (1 - conf) / conf
         omega = alpha * np.dot(np.dot(P,Sigma),P.T)
         return omega
 
+    def Meuccibl(self):
+        """This function performs the Black-Litterman blending of the prior
+        and the views into a new posterior estimate of the returns as
+        described in the various papers by Meucci.
+        ----------
+         Inputs
+           delta- Risk tolerance from the equilibrium portfolio
+           weq- Weights of the assets in the equilibrium portfolio
+           sigma- Prior covariance matrix
+           tau- Coefficiet of uncertainty in the prior estimate of the mean (pi)
+           P- Pick matrix for the view(s)
+           Q- Vector of view returns
+           Omega- Matrix of variance of the views (diagonal)
+         Outputs
+           Er - Posterior estimate of the mean returns
+           w- Unconstrained weights computed given the Posterior estimates
+              of the mean and covariance of returns.
+        """
+
+        #// Reverse optimize and back out the equilibrium returns
+        #// This is formula (12) page 6.
+        pi = np.dot(self.weq, self.sigma) * self.delta;
+        
+        #// We use tau * sigma many places so just compute it once
+        ts = self.tau * self.sigma;
+        #// Compute posterior estimate of the mean
+        #// This is a simplified version of formula (8) on page 4.
+        pi = np.expand_dims(pi ,axis=0)
+        P = np.expand_dims(self.P.T,axis=0)
+        er = pi.T + np.dot(ts , P.T) * linalg.inv(np.dot(np.dot(P, ts),P.T) + self.Omega) * (self.Q - np.dot(P , pi.T));
+        
+        #// Compute posterior weights based on uncertainty in mean
+        w = (np.dot(er.T , linalg.inv(self.delta * self.sigma))).T;
+        
+        res = namedtuple('res', ['er', 'w', 'delta', 'weq','V', 'tau', 'P', 'Q', 'Omega'])
+        res.er = er
+        res.w = w
+        res.delta = self.delta
+        res.V = self.sigma
+        res.weq = self.weq
+        res.P = self.P
+        res.Q = self.Q
+        res.Omega = self.Omega
+        return res
+      
 class display(Blacklitterman):
     
     def __init__(self, tau, P, Q, Omega, title, assets, res):
@@ -199,3 +246,24 @@ class display(Blacklitterman):
             i = i + 1
         print(line)
         print('------------------------------------------------------------------\n')
+        
+    def disppd(self, hPi, pi):
+        print(self.title)
+        weq = self.res.weq
+        V  = self.res.V
+        Er = self.res.er
+        
+        import pandas as pd
+        columns = ['Asset Name', 'Mkt Port (%)','Mean (%)','Std Dev (%)','BL Mean(%)','BL + View  Mean (%)', 'Investor Views'];
+        result = pd.DataFrame(columns = columns)
+        result['Asset Name'] = np.array(self.assets)
+        result['Mkt Port (%)'] = 100*weq
+        result['Mean (%)'] = 100*hPi
+        result['Std Dev (%)'] = 100*np.sqrt(np.diag(V))
+        result['BL Mean(%)'] = 100*pi
+        result['BL + View  Mean (%)'] = 100*Er
+        result['Investor Views'] = 100*self.P
+        result = result.set_index('Asset Name')
+        print(result.round(1).to_string())
+        print('\n')
+   
